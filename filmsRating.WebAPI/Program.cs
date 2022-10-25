3c6cb8fe327a12f492342cc7e9fecbb6a9be6550
Kisido <EnceladusKisido@gmail.com>
2022-10-26 01:06:56 +0300

    Add skeleton

---
+++ filmsRating.WebAPI/Program.cs
@@ -0,0 +1,40 @@
+using filmsRating.WebAPI.AppConfiguration.ServicesExtensions;
+using filmsRating.WebAPI.AppConfiguration.ApplicationExtensions;
+using Serilog;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.AddSerilogConfiguration();
+builder.Services.AddVersioningConfiguration();
+builder.Services.AddControllers();
+builder.Services.AddSwaggerConfiguration();
+
+var app = builder.Build();
+
+app.UseSerilogConfiguration();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwaggerConfiguration();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+try
+{
+    Log.Information("Application starting...");
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Error("Application finished with error {error}", ex);
+}
+finally
+{
+    Log.Information("Application stopped");
+    Log.CloseAndFlush();
+}
